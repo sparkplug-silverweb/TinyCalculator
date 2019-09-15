620fe4f831d83c7cbcdebcb2e25f0fb4e8439dab
lester.serdenia@gmail.com <lester.serdenia@gmail.com>
2019-09-15 23:19:51 +0800

    selectable

--- src/test/java/com/boilingpoint/tinycalculatortests/NewSeleneseIT.java
+++ src/test/java/com/boilingpoint/tinycalculatortests/NewSeleneseIT.java
@@ -8,10 +8,13 @@ package com.boilingpoint.tinycalculatortests;
 import junit.framework.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -174,5 +177,26 @@ public class NewSeleneseIT {
 
         driver.quit();
           
+    }
+    
+    
+    @Test
+    public void ActionBuildPerform(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://localhost:8080/TinyCalculator/index.xhtml");
+        WebElement one = driver.findElement(By.name("one"));
+        WebElement three = driver.findElement(By.name("three"));
+        WebElement five = driver.findElement(By.name("five"));
+        // Add all the actions into the Actions builder.
+        Actions builder = new Actions(driver);
+        builder.keyDown(Keys.CONTROL)
+            .click(one)
+            .click(three)
+            .click(five)
+            .keyUp(Keys.CONTROL);
+        // Generate the composite action.
+        Action compositeAction = builder.build();
+        // Perform the composite action.
+        compositeAction.perform();
     }
 }
